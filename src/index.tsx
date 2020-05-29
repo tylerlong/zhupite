37d0e61172810c9c006b9f48f3141d56189a60f9
Tyler Long <tyler4long@gmail.com>
2020-05-28 18:51:41 -0700

    Create main.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import App from './main';
+
 const container = document.createElement('div');
 document.body.appendChild(container);
-ReactDOM.render(<h1>Hello world</h1>, container);
+ReactDOM.render(<App />, container);
